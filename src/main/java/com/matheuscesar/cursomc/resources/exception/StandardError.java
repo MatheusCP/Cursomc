0bd58dd9ba71e749aeba501feaa0c4ca72fd3ff9
Matheus Cesar <pmatheus.cesar240@gmail.com>
2019-04-25 14:55:25 -0300

    Ajustes no endpoint /categorias/{id}

---
+++ src/main/java/com/matheuscesar/cursomc/resources/exception/StandardError.java
@@ -0,0 +1,46 @@
+package com.matheuscesar.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+import org.springframework.http.HttpStatus;
+
+public class StandardError implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private int status;
+	private String msg;
+	private long timeStamp;
+	
+	public StandardError(int status, String msg, long timeStamp) {
+		super();
+		this.status = status;
+		this.msg = msg;
+		this.timeStamp = timeStamp;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	public String getMsg() {
+		return msg;
+	}
+	
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
+	
+}
